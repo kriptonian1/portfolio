14ec667b7e8d22d8ef2796ace615afc9bbd2b5a6
Sawan Bhattacharya <shymal.sawan@gmail.com>
2026-06-09 23:51:23 +0530

    feat: add my books

---
+++ src/components/prespective-book/index.ts
@@ -0,0 +1,4 @@
+export { default as PrespectiveBook } from "./PrespectiveBook.astro";
+export { default as BookHeader } from "./BookHeader.astro";
+export { default as BookTitle } from "./BookTitle.astro";
+export { default as BookDescription } from "./BookDescription.astro";
